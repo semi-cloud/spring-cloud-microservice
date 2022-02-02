81a05dbeb37edf414567f53d529720e25f266320
semi_cloud <sksmsrkdtpal@naver.com>
2022-02-02 19:35:49 +0900

    feat: spring cloud gateway와 유레카 연동

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,21 @@
 package com.example.firstservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/first-service/")
 @Slf4j
 public class FirstServiceController {
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -23,7 +29,11 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request){
+        log.info("Server port={}", request.getServerPort());
+
+        //application.yml 랜덤 포트 번호 가져오기
+        return String.format("Hi, there. This is a message from First Service on PORT %s"
+                , env.getProperty("local.server.port"));
     }
 }
