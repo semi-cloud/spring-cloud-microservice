48cc6d7f2c701dfd09bdf4facca02441042142c7
semi_cloud <sksmsrkdtpal@naver.com>
2022-02-11 16:30:40 +0900

    feat: user service 와 spring cloud config 연동

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -27,9 +27,12 @@ public class UserController {
     @Autowired private Greeting greeting;
 
     @GetMapping("/health_check")
-    public String status(){
-        return String.format("It's Working in User Service on port %s",
-                env.getProperty("local.server.port"));
+    public String status() {
+        return String.format("It's Working in User Service"
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("/welcome")
--- user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
+++ user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -1,7 +1,6 @@
 package com.example.userservice.vo;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,7 +8,6 @@ import javax.validation.constraints.Size;
 public class RequestLogin {
     @NotNull(message = "Email cannot be null")
     @Size(min=2, message = "Email not be less than two characters")
-    @Email
     private String email;
 
     @NotNull(message = "Password cannot be null")
